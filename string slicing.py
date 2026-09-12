e7a12261ef11c9fbc1aedd6e47b2c624f49e5ce1
Catherine A. Nakayenga <nakayengacatherine@gmail.com>
2026-09-12 12:09:57 -0500

    Implement string slicing examples in string slicing.py
    Add string slicing examples for various cases.

---
+++ string slicing.py
@@ -0,0 +1,13 @@
+lname = "nakayenga"
+
+print(lname[::-1])
+print(lname[::2])
+print(lname[1::2])
+print(lname[1:5])
+print(lname[-1])
+print(lname[-3:])
+print(lname[:-3])
+print(lname[2:-2])
+print(lname[::-1])
+print(lname[::-2])
+print(lname[1:-1:2])
